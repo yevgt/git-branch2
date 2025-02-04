e77fae7c258ca2364ac4f90a254db0f4910cd199
ICH <yev.guta@gmail.com>
2025-02-04 11:53:23 +0100

    add python file 'test 2 (25 02 04)'

---
+++ test 2 (25 - 02-04).py
@@ -0,0 +1,16 @@
+print(hash("hello"))
+print(hash("hello"))
+print(hash(100))
+
+
+print(hash(1))
+print(hash(True))
+# 1
+print(hash(1.0))
+# 1
+print(hash('1'))
+# -3723884734378080930
+print(hash('строка'))
+# -295037195106125010
+print(hash((1,2,3)))
+# 2528502973977326415
